ea3d018ca6c21d5245d0f4ca8a89927539ba3224
Ethan Arbynov <terdem@byu.edu>
2026-03-23 10:03:58 -0600

    Phase 5, commit 7. Register and login tests

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -3,16 +3,24 @@ package client;
 import org.junit.jupiter.api.*;
 import server.Server;
 
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ServerFacadeTests {
 
     private static Server server;
+    private static ServerFacade facade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+        facade = new ServerFacade("http://localhost:" + port);
+    }
+
+    @BeforeEach
+    public void clearDatabase() throws Exception {
+        facade.clear();
     }
 
     @AfterAll
@@ -20,10 +28,27 @@ public class ServerFacadeTests {
         server.stop();
     }
 
-
     @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
+    public void registerPositive() throws Exception {
+        String authToken = facade.register("player1", "password", "p1@email.com");
+        assertNotNull(authToken);
+        assertFalse(authToken.isBlank());
     }
 
+    @Test
+    public void registerNegative() throws Exception {
+        facade.register("player1", "password", "p1@email.com");
+
+        assertThrows(ClientException.class, () ->
+                facade.register("player1", "password", "p1@email.com"));
+    }
+
+    @Test
+    public void loginPositive() throws Exception {
+        facade.register("player1", "password", "p1@email.com");
+
+        String authToken = facade.login("player1", "password");
+        assertNotNull(authToken);
+        assertFalse(authToken.isBlank());
+    }
 }
